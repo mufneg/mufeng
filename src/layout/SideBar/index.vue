bb70e88a068b88f64056c37c433c20ebd372c6b0
xjw <17629520972@163.com>
2024-12-25 17:11:18 +0800

    组件加代码【无功能】

---
+++ src/layout/SideBar/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { onMounted } from "vue";
+const template = () => {};
+
+onMounted(() => {});
+</script>
+<template>
+  <div></div>
+</template>
+
+<style scoped lang="scss"></style>
